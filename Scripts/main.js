9e95b6c2bc5b98ed6b72a5c7c5adb4b145f3f7a1
Walter Wlodarski <krishmaratalata@gmail.com>
2026-09-15 13:56:54 -0400

    étap 7

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,7 +2,7 @@ import './app.js';
 
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('../sw.js')
+		navigator.serviceWorker.register('./sw.js')
 			.catch((error) => console.error('Échec de l’enregistrement du service worker:', error));
 	});
 }

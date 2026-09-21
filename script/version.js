11952640c8d5fc4e0b5d029bfd69ee1a2397af45
Walter Wlodarski <krishmaratalata@gmail.com>
2026-09-21 18:39:29 -0400

    2026.09.21 GPS/GLO-90/OSM/BLOB

--- script/version.js
+++ script/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '2026.09.21 GPS/GLO-90/OSM';
+export const APP_VERSION = '2026.09.21 GPS/GLO-90/OSM/BLOB';
 
 export function getVersionLabel() {
   return `version ${APP_VERSION}`;

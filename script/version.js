ddaf3f1561d1ffa23ad918e8d1cf8227c76377a9
Walter Wlodarski <krishmaratalata@gmail.com>
2026-09-23 08:54:24 -0400

    2026.09.23 WX3/GLO-90/OSM X:ORG

--- script/version.js
+++ script/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '2026.09.23 WX3/GLO-90/OSM X:TOL.BUG';
+export const APP_VERSION = '2026.09.23 WX3/GLO-90/OSM X:ORG';
 
 export function getVersionLabel() {
   return `version ${APP_VERSION}`;

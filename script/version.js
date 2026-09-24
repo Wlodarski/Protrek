fb1e4005eaa091c3bd93c9c520a1db9fd743601c
Walter Wlodarski <krishmaratalata@gmail.com>
2026-09-24 13:32:21 -0400

    2026.09.24 WX3/GLO-90/OSM

--- script/version.js
+++ script/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '2026.09.23 WX3/GLO-90/OSM';
+export const APP_VERSION = '2026.09.24 WX3/GLO-90/OSM';
 
 /**
  * Retourne l'étiquette de version à afficher dans l'interface.
